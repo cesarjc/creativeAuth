b03a8384340124abfaee36602d27870730d4c0fc
Ubuntu <cesarjuliocm@gmail.com>
2017-12-12 19:06:31 +0000

    added models

---
+++ db.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/messenger', { useMongoClient: true, promiseLibrary: global.Promise })
+  .then(() => console.log('connected'))
+  .catch(console.error);
+
+const Message = mongoose.model('Message', mongoose.Schema({ from: String, message: String, to: String }));
+const create = (messageInfo) => new Message(messageInfo).save();
+const del = (query) => Message.remove(query).exec();
+
+const read = (query) => new Promise((resolve, reject) => {
+  const recievedMessagesQuery = { from: query.to, to: query.from };
+  Message.find({ $or: [query, recievedMessagesQuery] }, (error, messages) => error ? reject(error) : resolve(messages));
+});
+
+module.exports = { create, read, del };
---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const db = require('./db');
+const app = express();
+
+const handleError = (error, res) => {
+  console.error(error);
+  res.sendStatus(500);
+};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+app.post('/message', (req, res) => {
+  db.create(req.body)
+    .then(() => res.sendStatus(200))
+    .catch((error) => handleError(error, res));
+});
+
+app.get('/message', (req, res) => {
+  db.read(req.query)
+    .then((messages) => res.json(messages))
+    .catch((error) => handleError(error, res));
+});
+
+app.delete('/message', (req, res) => {
+  db.del(req.query)
+    .then(() => res.sendStatus(200))
+    .catch((error) => handleError(error, res));
+});
+
